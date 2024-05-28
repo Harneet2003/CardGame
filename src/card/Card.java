cb78aed87653ced69c757cf6a8898b6a0fb92950
harneet2003 <kau16367@sheridancollege.ca>
2024-05-27 21:05:25 -0400

    card game

--- src/card/Card.java
+++ src/card/Card.java
@@ -4,6 +4,8 @@
  */
 package card;
 
+import java.util.Objects;
+
 /**
  * A class that models playing card Objects. Cards have 
  * a value (note that Ace = 1, Jack -11, Queen =12, King = 13)
@@ -46,6 +48,34 @@ public class Card {
     public void setValue(int value) {
         this.value = value;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 29 * hash + Objects.hashCode(this.suit);
+        hash = 29 * hash + this.value;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Card other = (Card) obj;
+        if (this.value != other.value) {
+            return false;
+        }
+        return Objects.equals(this.suit, other.suit);
+    }
+    
+    
    
    
     
--- src/card/CardTrick.java
+++ src/card/CardTrick.java
@@ -2,7 +2,15 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+
+/*Harneet Kaur
+Student Number: 991728683
+*/
 package card;
+
+import static card.Card.SUITS;
+import java.util.Random;
+import java.util.Scanner;
 
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
@@ -19,8 +27,41 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            
+            Random rn=new Random();
+            
+            c.setValue(rn.nextInt(13)+1);
+            c.setSuit(Card.SUITS[rn.nextInt(4)]);
+            magicHand[i]=c;
+        }
+        
+        for(Card card:magicHand){
+            System.out.print(card.getSuit()+" ");
+            System.out.println(card.getValue());
+        }
+
+        
+        Scanner input=new Scanner(System.in);
+        System.out.print("Enter a card value(1-13): ");
+        int value=input.nextInt();
+        System.out.print("Enter a card suit(0-3 where 0=Hearts, 1=Diamonds, 2=Spades, 3=Clubs): ");
+        int suit=input.nextInt();
+        
+        String userSuit = SUITS[suit];
+
+        boolean found=false;
+        for(Card card:magicHand){
+            
+            if(card.getValue()==value && card.getSuit().equalsIgnoreCase(userSuit)){
+                found=true;
+            }
+        }
+        
+        if(found){
+            System.out.println("Your card is in the magic hand");
+        }
+        else{
+            System.out.println("Sorry, your is not in the magic hand");
         }
         
         //insert code to ask the user for Card value and suit, create their card
@@ -28,5 +69,5 @@ public class CardTrick {
         //Then report the result here
         // add one luckcard hard code 2,clubs
     }
-    
+
 }
